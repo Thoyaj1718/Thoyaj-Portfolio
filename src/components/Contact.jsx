ae219696b21cee2a93637a35a84bbd293d45dfe4
Thoyaj1718 <thoyajkumar1718@gmail.com>
2025-11-26 19:13:30 +0530

    Save work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
             </div>
             <div className='mb-4'>
                 <FaMapMarkedAlt className='inline-block text-green-400 mr-2'></FaMapMarkedAlt>
-                <span>sreet, city, province, country</span>
+                <span>Tirupati, Andhra Pradesh, India</span>
             </div>
           </div>
           <div className='flex-1 w-full'>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className="flex-1 mb-4 md:mb-0">
             <h3 className="text-2xl font-bold mb-2">Thoyaj Kumar</h3>
             <p className="text-gray-400">
-              Full-Stack Developer based in the USA, specializing in web and
+              Full-Stack Developer, specializing in web and
               software development.
             </p>
           </div>
